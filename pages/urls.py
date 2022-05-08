f4c57de81a6d0d85bab0b2f07c7fb5be5d179ac8
admin <nailfax1991@yandex.ru>
2022-05-08 22:42:46 +0200

    удалены лишние файлы

--- pages/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import HomePageView, AboutPageView # новое
-
-urlpatterns = [
-    path('about/', AboutPageView.as_view(), name='about'), # новое
-    path('', HomePageView.as_view(), name='home'),
-]
